b1e246d38f4cf98c55714aa8a113a9453cb28fb4
MacBook <macbook@MacBooks-MacBook-Air.local>
2024-05-12 05:18:30 +0700

    test

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <input type="text" placeholder="email" v-model="userCredentials.email">
+        <input type="password" placeholder="password" v-model="userCredentials.password">
+        <button @click="loginUser">Login</button>
+
+        <div>{{ user }}</div>
+    </div>
+</template>
+<script setup>
+const { login } = useSanctumAuth();
+
+const user = useSanctumUser();
+
+console.log(user.value);
+
+const userCredentials = {
+    email: '',
+    password: '',
+};
+
+async function loginUser() {
+    await login(userCredentials);
+}
+</script>
